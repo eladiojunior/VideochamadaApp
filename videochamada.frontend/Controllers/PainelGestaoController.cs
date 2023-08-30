77a514a6e7c3ce68f0089ec44950a32d3f1af423
eladio.junior <eladio.junior@cassi.com.br>
2023-08-30 18:51:11 -0300

    Subindo area de Painel de Gestão.

---
+++ videochamada.frontend/Controllers/PainelGestaoController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VideoChatApp.FrontEnd.Controllers;
+
+public class PainelGestaoController : GenericController
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
